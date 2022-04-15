2195ed7996b1367f32e7dae2a5f14d89f807278a
João Paulo Nunes <jpnune@gmail.com>
2022-04-15 18:31:38 -0300

    .

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,5 @@
-from importlib.resources import path
-
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-
-def contact(request):
-    return HttpResponse('Contact')
-
-
-def about(request):
-    return HttpResponse('About')
